b14063223a2c83ef4c8b9a10e292c166d6694c67
Aleksandr Lukyanovich <lukjanvich@gmail.com>
2026-06-30 12:04:50 +0300

    Initial Spring Boot project

---
+++ src/test/java/io/github/alexlukyanovich/careerpilot/CareerPilotAiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alexlukyanovich.careerpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerPilotAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
